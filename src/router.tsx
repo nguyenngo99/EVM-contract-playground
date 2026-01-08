8913c1ac4a67eb77de19ea591061d3e31ea82a0f
nguyenngo99 <ngokhoaisan1999@gmail.com>
2026-01-08 16:41:21 +0700

    feat: 🎸 Update default router

--- src/router.tsx
+++ src/router.tsx
@@ -17,7 +17,7 @@ const rootRoute = createRootRoute({
 const indexRoute = createRoute({
   getParentRoute: () => rootRoute,
   path: '/',
-  component: () => <UsdcBalance publicClient={publicClient} />,
+  component: () => <ContractPlayground publicClient={publicClient} />,
 });
 
 const playgroundRoute = createRoute({
